5bc43c17be1c767a4643e4b696ac8e01e380e9c8
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-27 02:12:17 +0800

    feat: 用户登入注册 DTO 引入

---
+++ admin/src/main/java/org/liquidice/exmall/admin/dto/req/UserRegisterReqDTO.java
@@ -0,0 +1,29 @@
+package org.liquidice.exmall.admin.dto.req;
+
+import lombok.Data;
+
+/**
+ * 用户注册请求DTO
+ */
+@Data
+public class UserRegisterReqDTO {
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
---
+++ admin/src/main/java/org/liquidice/exmall/admin/dto/resp/UserRespDTO.java
@@ -0,0 +1,32 @@
+package org.liquidice.exmall.admin.dto.resp;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+import org.liquidice.exmall.framework.serialize.PhoneDesensitizationSerializer;
+
+/**
+ * 用户返回参数响应
+ */
+@Data
+public class UserRespDTO {
+    /**
+     * uid
+     */
+    private Long uid;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 手机号
+     */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+}
