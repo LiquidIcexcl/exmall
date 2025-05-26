c8126ad0d46a2a34516d0fa79e9f8cfce3994db6
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-26 20:18:29 +0800

    first commit!

---
+++ src/main/java/org/liquidice/exmall/ExmallApplication.java
@@ -0,0 +1,13 @@
+package org.liquidice.exmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExmallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/liquidice/exmall/ExmallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liquidice.exmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
