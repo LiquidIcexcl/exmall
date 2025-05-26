29c91716c4f0d955c0b4b3bdd95e019e2cd49f6e
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-26 20:48:45 +0800

    feat：公共组件-全局异常约束|全局结果约束

---
+++ framework/src/main/java/org/liquidice/exmall/framework/errorcode/IErrorCode.java
@@ -0,0 +1,18 @@
+package org.liquidice.exmall.framework.errorcode;
+
+
+/**
+ * 平台错误码接口
+ */
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
