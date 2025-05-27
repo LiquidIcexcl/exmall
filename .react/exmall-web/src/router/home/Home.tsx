dea5ddc7c937f09f4ff9342e1d1ceb6f5aac16be
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-28 03:48:22 +0800

    build: 前端-首页预制作

---
+++ .react/exmall-web/src/router/home/Home.tsx
@@ -0,0 +1,23 @@
+// import React from'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <>
+        <div className="home">
+            <h1>Welcome to Exmall</h1>
+            <p>Your one-stop shop for everything!</p>
+            <div className="features">
+            <h2>Features</h2>
+            <ul>
+                <li>Wide range of products</li>
+                <li>Easy navigation</li>
+                <li>Secure checkout</li>
+            </ul>
+            </div>
+        </div>
+    </>
+  );
+};
+
+export default Home;
