5a72b6299ab10d0ef765131767f5a3583bbca0b6
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-30 03:47:55 +0800

    feat: Pay实体类初始化

--- pay/src/main/java/org/liquidice/exmall/pay/dto/req/payReqDTO.java
+++ pay/src/main/java/org/liquidice/exmall/pay/dto/req/payReqDTO.java
@@ -8,4 +8,44 @@ import lombok.Data;
  */
 @Data
 public class payReqDTO {
+
+    /**
+     * 支付表自增主键ID-支付流水号
+     */
+    private Long payId;
+
+    /**
+     * 支付类型：收入或支出
+     */
+    private String payType; // "income"|"outcome"
+
+    /**
+     * 付款人UID
+     */
+    private Long payerUid;
+
+    /**
+     * 收款人UID
+     */
+    private Long payeeUid;
+
+    /**
+     * 订单ID（业务层面标识订单的编号）
+     */
+    private Long orderId;
+
+    /**
+     * 支付渠道
+     */
+    private String payChannel; // "WeChatPay"|"AliPay"|"WalletPay"|"CDKPay"
+
+    /**
+     * 支付状态
+     */
+    private Integer payState = 0; //支付状态：0:待支付 | 1:已支付 | 2:支付失败
+
+    /**
+     * 支付金额
+     */
+    private Long payAmount;
 }
