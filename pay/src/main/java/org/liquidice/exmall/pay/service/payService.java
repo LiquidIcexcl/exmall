6c651f150f8083810f216e5a0013048c3bef32c7
Koishi Hsiao <9109222172@email.ncu.edu.cn>
2025-05-30 03:48:07 +0800

    feat: Pay服务层初始化

--- pay/src/main/java/org/liquidice/exmall/pay/service/payService.java
+++ pay/src/main/java/org/liquidice/exmall/pay/service/payService.java
@@ -1,4 +1,7 @@
 package org.liquidice.exmall.pay.service;
 
-public interface payService {
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.liquidice.exmall.pay.dao.entity.payDO;
+
+public interface payService extends IService<payDO> {
 }
